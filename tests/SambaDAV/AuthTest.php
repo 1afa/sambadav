0d5ca543027d484cd3c19021bfeae0228b791edb
Alfred Klomp <alfred@1afa.com>
2014-12-30 11:30:29 +0100

    AuthTest: shorten by using dataProvider

--- tests/SambaDAV/AuthTest.php
+++ tests/SambaDAV/AuthTest.php
@@ -5,94 +5,35 @@ namespace SambaDAV;
 class AuthTest extends \PHPUnit_Framework_TestCase
 {
 	public function
-	testFillPattern_A ()
+	fillPatternProvider ()
 	{
-		$auth = new Auth(null);
-		$auth->user = 'foo\\bar@baz';
-		$this->assertEquals('foo', $auth->fillPattern('%w'));
+		return
+		[ [ 'foo\\bar@baz', '%w', 'foo' ]
+		, [ 'foo\\bar@baz', '%u', 'bar' ]
+		, [ 'foo\\bar@baz', '%d', 'baz' ]
+		, [ 'foo\\bar',     '%u', 'bar' ]
+		, [ 'foo\\bar',     '%d', '' ]
+		, [ 'bar@',         '%w', '' ]
+		, [ 'bar@',         '%u', 'bar' ]
+		, [ 'bar@',         '%d', '' ]
+		, [ 'foo\\bar',     '%d %u %w', false ]
+		, [ '',             '%d %u %w', false ]
+		, [ 'foo\\@bar',    '%u', '' ]
+		, [ '\\f@',         '%u', 'f' ]
+		, [ 'f@',           '%u', 'f' ]
+		, [ '\\f',          '%u', 'f' ]
+		] ;
 	}
 
+	/**
+	 * @dataProvider fillPatternProvider
+	 */
 	public function
-	testFillPattern_B ()
+	testFillPattern ($user, $pattern, $expect)
 	{
 		$auth = new Auth(null);
-		$auth->user = 'foo\\bar@baz';
-		$this->assertEquals('bar', $auth->fillPattern('%u'));
-	}
-
-	public function
-	testFillPattern_C ()
-	{
-		$auth = new Auth(null);
-		$auth->user = 'foo\\bar@baz';
-		$this->assertEquals('baz', $auth->fillPattern('%d'));
-	}
-
-	public function
-	testFillPattern_D ()
-	{
-		$auth = new Auth(null);
-		$auth->user = 'foo\\bar';
-		$this->assertEquals('bar', $auth->fillPattern('%u'));
-		$this->assertEquals('', $auth->fillPattern('%d'));
-	}
-
-	public function
-	testFillPattern_E ()
-	{
-		$auth = new Auth(null);
-		$auth->user = 'bar@';
-		$this->assertEquals('', $auth->fillPattern('%w'));
-		$this->assertEquals('bar', $auth->fillPattern('%u'));
-		$this->assertEquals('', $auth->fillPattern('%d'));
-	}
-
-	public function
-	testFillPattern_F ()
-	{
-		$auth = new Auth(null);
-		$auth->user = 'foo\\bar';
-		$this->assertEquals(false, $auth->fillPattern('%d %u %w'));
-	}
-
-	public function
-	testFillPattern_G ()
-	{
-		$auth = new Auth(null);
-		$auth->user = '';
-		$this->assertEquals(false, $auth->fillPattern('%d %u %w'));
-	}
-
-	public function
-	testFillPattern_H ()
-	{
-		$auth = new Auth(null);
-		$auth->user = 'foo\\@bar';
-		$this->assertEquals('', $auth->fillPattern('%u'));
-	}
-
-	public function
-	testFillPattern_I ()
-	{
-		$auth = new Auth(null);
-		$auth->user = '\\f@';
-		$this->assertEquals('f', $auth->fillPattern('%u'));
-	}
-
-	public function
-	testFillPattern_J ()
-	{
-		$auth = new Auth(null);
-		$auth->user = 'f@';
-		$this->assertEquals('f', $auth->fillPattern('%u'));
-	}
-
-	public function
-	testFillPattern_K ()
-	{
-		$auth = new Auth(null);
-		$auth->user = '\\f';
-		$this->assertEquals('f', $auth->fillPattern('%u'));
+		$auth->user = $user;
+		$this->assertEquals($expect, $auth->fillPattern($pattern));
 	}
 
 	public function
